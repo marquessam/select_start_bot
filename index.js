f7dcd51b49c96c397e83c15f1376c4037dceb17a
marquessam <samuelnmarques@gmail.com>
2024-12-21 19:01:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,30 @@ client.on('messageCreate', async message => {
     }
 
     // Challenge command
+  // Challenge command
     if (message.content === '!challenge') {
+        // First send a console-style prefix
+        await message.channel.send('```ansi\n\x1b[32m> Accessing challenge database...\x1b[0m\n```');
+
         const embed = new EmbedBuilder()
-            .setColor('#5c3391')
-            .setTitle('December 2024 Challenge')
-            .setDescription('Final Fantasy Tactics: The War of the Lions');
+            .setColor('#00FF00')
+            .setTitle('████ SELECT START MONTHLY CHALLENGE ████')
+            .setDescription('```ansi\n\x1b[32m[CHALLENGE STATUS: ACTIVE]\n[PERIOD: 12.01.2024 - 12.31.2024]\x1b[0m```')
+            .addFields(
+                { 
+                    name: '`MISSION`', 
+                    value: '```\nComplete achievements in Final Fantasy Tactics: The War of the Lions```' 
+                },
+                { 
+                    name: '`PARAMETERS`', 
+                    value: '```\n- Hardcore mode required\n- All achievements eligible\n- Progress tracked via RetroAchievements.org\n- Multiplayer tiebreaker system active```' 
+                }
+            );
             
-        await message.reply({ embeds: [embed] });
+        await message.channel.send({ embeds: [embed] });
+
+        // Send a follow-up console prompt
+        await message.channel.send('```ansi\n\x1b[32m> Use !leaderboard to view current rankings...\x1b[0m█\n```');
     }
 });
 
