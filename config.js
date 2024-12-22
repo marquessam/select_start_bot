d0f32f5930db4eac9809d38e80256cb47c530add
marquessam <samuelnmarques@gmail.com>
2024-12-22 09:52:19 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    currentChallenge: {
+        gameId: '3236',
+        gameName: 'Final Fantasy Tactics: The War of the Lions',
+        gameIcon: '/Images/074335.png',
+        startDate: '12.01.2024',
+        endDate: '12.31.2024',
+        rules: [
+            'Hardcore mode required',
+            'All achievements eligible',
+            'Progress tracked via RetroAchievements.org',
+            'Multiplayer tiebreaker system active'
+        ],
+        points: {
+            first: 10,
+            second: 6,
+            third: 3
+        }
+    }
+};
