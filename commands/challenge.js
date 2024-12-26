33699e3e4ac71d70071c29fbc2f7164e43384209
marquessam <samuelnmarques@gmail.com>
2024-12-26 20:10:33 +0800

    Update challenge.js

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,14 +1,37 @@
-const { EmbedBuilder } = require('discord.js');
+const TerminalEmbed = require('../utils/embedBuilder');
+const { getCurrentChallenge } = require('../challengeConfig.js');
 
 module.exports = {
     name: 'challenge',
-    description: 'Shows current monthly challenge',
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setColor('#00FF00')
-            .setTitle('SELECT START MONTHLY CHALLENGE')
-            .setDescription('December 2024 Challenge');
+    description: 'Displays current monthly challenge',
+    async execute(message, args, { shadowGame }) {
+        try {
+            await message.channel.send('```ansi\n\x1b[32m> Accessing challenge database...\x1b[0m\n```');
 
-        await message.channel.send({ embeds: [embed] });
-    },
+            const currentChallenge = await getCurrentChallenge();
+
+            const embed = new TerminalEmbed()
+                .setTerminalTitle('MONTHLY CHALLENGE')
+                .setURL(`https://retroachievements.org/game/${currentChallenge.gameId}`)
+                .setThumbnail(`https://retroachievements.org${currentChallenge.gameIcon}`)
+                .setTerminalDescription('[STATUS: ACTIVE]\n[DATA VERIFIED]')
+                .addTerminalField('CURRENT CHALLENGE', currentChallenge.gameName)
+                .addTerminalField('CHALLENGE TIMEFRAME', 
+                    `${currentChallenge.startDate} - ${currentChallenge.endDate}`)
+                .addTerminalField('CHALLENGE PARAMETERS', 
+                    currentChallenge.rules.map(rule => `> ${rule}`).join('\n'))
+                .addTerminalField('REWARD PROTOCOL',
+                    `> 🥇 ${currentChallenge.points.first} pts\n` +
+                    `> 🥈 ${currentChallenge.points.second} pts\n` +
+                    `> 🥉 ${currentChallenge.points.third} pts`)
+                .setTerminalFooter();
+            
+            await message.channel.send({ embeds: [embed] });
+            await message.channel.send('```ansi\n\x1b[32m> Type !leaderboard to view current rankings\n[Ready for input]█\x1b[0m```');
+            await shadowGame.tryShowError(message);
+        } catch (error) {
+            console.error('Challenge Error:', error);
+            await message.channel.send('```ansi\n\x1b[32m[ERROR] Mission data inaccessible\n[Ready for input]█\x1b[0m```');
+        }
+    }
 };
