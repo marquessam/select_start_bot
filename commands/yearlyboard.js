2cf4839999171745c990f05bb0f4078e884cef54
marquessam <samuelnmarques@gmail.com>
2024-12-29 14:10:59 +0800

    Update yearlyboard.js

--- commands/yearlyboard.js
+++ commands/yearlyboard.js
@@ -1,3 +1,4 @@
+// commands/yearlyboard.js
 const TerminalEmbed = require('../utils/embedBuilder');
 
 module.exports = {
@@ -7,40 +8,23 @@ module.exports = {
         try {
             await message.channel.send('```ansi\n\x1b[32m> Accessing yearly rankings...\x1b[0m\n```');
             
+            // Get valid users and yearly leaderboard
+            const validUsers = await userStats.getAllUsers();
             const leaderboard = await userStats.getYearlyLeaderboard();
+            
+            // Filter leaderboard to only include valid users
+            const filteredLeaderboard = leaderboard.filter(user => 
+                validUsers.includes(user.username)
+            );
             
             const embed = new TerminalEmbed()
                 .setTerminalTitle('YEARLY RANKINGS')
-                .setTerminalDescription('[DATABASE ACCESS GRANTED]\n[DISPLAYING CURRENT STANDINGS]');
-
-            // Format all participants
-            const leaderboardText = leaderboard
-                .map((user, index) => `${index + 1}. ${user.username}: ${user.points} points`)
-                .join('\n');
-
-            // Split into multiple fields if needed (Discord's 1024 character limit per field)
-            const chunks = [];
-            let currentChunk = '';
-            
-            leaderboardText.split('\n').forEach(line => {
-                if (currentChunk.length + line.length + 1 > 1000) {
-                    chunks.push(currentChunk);
-                    currentChunk = line;
-                } else {
-                    currentChunk += (currentChunk ? '\n' : '') + line;
-                }
-            });
-            if (currentChunk) chunks.push(currentChunk);
-
-            // Add each chunk as a separate field
-            chunks.forEach((chunk, index) => {
-                embed.addTerminalField(
-                    index === 0 ? 'RANKINGS' : 'RANKINGS (CONTINUED)',
-                    chunk
-                );
-            });
-
-            embed.setTerminalFooter();
+                .setTerminalDescription('[DATABASE ACCESS GRANTED]\n[DISPLAYING CURRENT STANDINGS]')
+                .addTerminalField('TOP OPERATORS',
+                    filteredLeaderboard
+                        .map((user, index) => `${index + 1}. ${user.username}: ${user.points} points`)
+                        .join('\n'))
+                .setTerminalFooter();
             
             await message.channel.send({ embeds: [embed] });
             await message.channel.send('```ansi\n\x1b[32m> Type !profile <user> for detailed stats\n[Ready for input]█\x1b[0m```');
