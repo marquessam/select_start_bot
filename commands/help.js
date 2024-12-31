366035bdd3ad5ceaa830468a28ffbb52244bb93a
marquessam <samuelnmarques@gmail.com>
2024-12-31 21:11:28 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,19 +9,19 @@ module.exports = {
         const embed = new TerminalEmbed()
             .setTerminalTitle('SELECT START TERMINAL')
             .setTerminalDescription(
-                'AVAILABLE COMMANDS:\n\n' +
+                '\x1b[37mAVAILABLE COMMANDS:\x1b[0m\n\n' +
                 '\x1b[37m=== CHALLENGE INFO ===\x1b[0m\n' +
-                '!challenge - Display current challenge\n' +
-                '!leaderboard - Display rankings (monthly, yearly, highscores)\n' +
-                '!nominations - Display nominated games\n\n' +
+                '\x1b[32m!challenge\x1b[0m - Display current challenge\n' +
+                '\x1b[32m!leaderboard\x1b[0m - Display rankings (monthly, yearly, highscores)\n' +
+                '\x1b[32m!nominations\x1b[0m - Display nominated games\n\n' +
                 '\x1b[37m=== USER STATS ===\x1b[0m\n' +
-                '!profile <user> - Access user achievement data\n' +
-                '!viewarchive <month> - View historical rankings\n' +
-                '\n\x1b[37m=== HIGH SCORES ===\x1b[0m\n' +
-                '!highscores - View all game high scores\n\n' +
+                '\x1b[32m!profile <user>\x1b[0m - Access user achievement data\n' +
+                '\x1b[32m!viewarchive <month>\x1b[0m - View historical rankings\n\n' +
+                '\x1b[37m=== HIGH SCORES ===\x1b[0m\n' +
+                '\x1b[32m!highscores\x1b[0m - View all game high scores\n\n' +
                 '\x1b[37m=== GENERAL ===\x1b[0m\n' +
-                '!help - Display available commands\n' +
-                '\n[Ready for input]█'
+                '\x1b[32m!help\x1b[0m - Display available commands\n\n' +
+                '\x1b[32m[Ready for input]█\x1b[0m'
             )
             .setTerminalFooter();
 
