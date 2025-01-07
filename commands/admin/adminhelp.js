803fe30ef3e68a0faf7381b52f835bab0f304e2c
marquessam <samuelnmarques@gmail.com>
2025-01-07 10:45:15 +0800

    Update adminhelp.js

--- commands/admin/adminhelp.js
+++ commands/admin/adminhelp.js
@@ -27,7 +27,7 @@ module.exports = {
 
                 '\x1b[37m=== ARCADE MANAGEMENT ===\x1b[0m\n' +
                 '\x1b[32m!arcade reset <game_number> [username]\x1b[0m\nReset game scores\n' +
-                '\x1b[32m!arcade rules\x1b[0m\nUpdate game rules\n\n'
+                '\x1b[32m!arcade rules\x1b[0m\nUpdate game rules\n\n' +
 
                 '\x1b[37m=== USER MANAGEMENT ===\x1b[0m\n' +
                 '\x1b[32m!removeuser <username>\x1b[0m\nRemove a user from the system\n\n' +
