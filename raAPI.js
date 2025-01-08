12e180f379609a4218179dced2037c04c227fce4
marquessam <samuelnmarques@gmail.com>
2025-01-08 09:35:05 +0800

    Update raAPI.js

--- raAPI.js
+++ raAPI.js
@@ -152,7 +152,7 @@ async function fetchLeaderboardData() {
                         completedAchievements: completed,
                         totalAchievements: numAchievements,
                         completionPercentage: numAchievements > 0 ? ((completed / numAchievements) * 100).toFixed(2) : "0.00",
-                        hasBeatenGame: hasBeatenGame
+                        hasBeatenGame: hasBeatenGame,
                         achievements: achievements
 });
 
