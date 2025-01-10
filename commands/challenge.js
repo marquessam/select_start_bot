7e3a3b0d97da7a94d98b5aca6e3a33fa3a7a9740
marquessam <samuelnmarques@gmail.com>
2025-01-10 09:39:20 +0800

    Update challenge.js

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,5 +1,5 @@
-import TerminalEmbed from('../utils/embedBuilder.js');
-import database from('../database');
+import TerminalEmbed from '../utils/embedBuilder.js';
+import database from '../database';
 
 export default {
     name: 'challenge',
