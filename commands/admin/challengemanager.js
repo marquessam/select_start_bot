f7e1300f5e010afef3202839009c6d8443eb253f
marquessam <samuelnmarques@gmail.com>
2025-01-10 09:32:28 +0800

    Update challengemanager.js

--- commands/admin/challengemanager.js
+++ commands/admin/challengemanager.js
@@ -1,7 +1,7 @@
-const TerminalEmbed = require('../../utils/embedBuilder');
-const database = require('../../database');
+import TerminalEmbed = require('../../utils/embedBuilder.js');
+import database = require('../../database.js');
 
-module.exports = {
+export default = {
     name: 'challengemanager',
     description: 'Manage monthly challenges',
     async execute(message, args, { announcer }) {
