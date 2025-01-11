cef4a5a0b20329e17454342103a0ac7bf9cde02c
marquessam <samuelnmarques@gmail.com>
2025-01-11 15:34:57 +0800

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -42,13 +42,16 @@ module.exports = {
         return description.replace(/<[^>]*>/g, '').trim();
       };
 
+      // Build the MobyGames game link
+      const gameLink = `https://www.mobygames.com/game/${game.id}/${game.title.replace(/\s+/g, '-').toLowerCase()}`;
+
       // 6) Build an embed (terminal-style) with data from the first game
       const embed = new TerminalEmbed()
         .setTerminalTitle(game.title)
         .setTerminalDescription(
           game.description
-            ? sanitizeDescription(game.description.slice(0, 300)) + '... [Read more](https://www.mobygames.com/game/' + game.id + ')'
-            : 'No description available. [More Info](https://www.mobygames.com/game/' + game.id + ')'
+            ? sanitizeDescription(game.description.slice(0, 300)) + `... [Read more](${gameLink})`
+            : `No description available. [More Info](${gameLink})`
         );
 
       // 7) Add a large image if it exists (instead of a thumbnail)
@@ -56,7 +59,7 @@ module.exports = {
         embed.setImage(game.sample_cover.image);
       }
 
-      // 8) Show some extra details (platforms, genres, etc.)
+      // 8) Show some extra details (platforms, genres, developer, publisher, etc.)
       if (Array.isArray(game.platforms) && game.platforms.length > 0) {
         const platformList = game.platforms
           .map((p) => `${p.platform_name} (${p.first_release_date || 'N/A'})`)
@@ -67,6 +70,14 @@ module.exports = {
       if (Array.isArray(game.genres) && game.genres.length > 0) {
         const genreList = game.genres.map((g) => g.genre_name).join(', ');
         embed.addTerminalField('Genres', genreList);
+      }
+
+      if (game.developer) {
+        embed.addTerminalField('Developer', game.developer);
+      }
+
+      if (game.publisher) {
+        embed.addTerminalField('Publisher', game.publisher);
       }
 
       // 9) Attribution message
@@ -87,3 +98,4 @@ module.exports = {
     }
   },
 };
+
