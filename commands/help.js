869c4a9c46f1d85bef719d6b4449cfc6cb894f53
marquessam <samuelnmarques@gmail.com>
2025-01-11 22:18:10 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,25 +11,36 @@ module.exports = {
             .setTerminalTitle('SELECT START TERMINAL')
             .setTerminalDescription(
                 '\x1b[37m=== CHALLENGE INFO ===\x1b[0m\n' +
-                '\x1b[32m!challenge - Display current monthly challenge\x1b[0m\n' +
-                '\x1b[32m!leaderboard - View leaderboards (month/year)\x1b[0m\n' +
-                '\x1b[32m!nominations - Display nominated games\x1b[0m\n\n' +
+                '\x1b[32m!challenge\x1b[0m - Display current monthly challenge\n' +
+                '\x1b[32m!leaderboard\x1b[0m - View leaderboards (month/year)\n' +
+                '\x1b[32m!nominations\x1b[0m - Display nominated games\n\n' +
+
                 '\x1b[37m=== USER STATS ===\x1b[0m\n' +
-                '\x1b[32m!profile <user> - View user stats and progress\x1b[0m\n' +
-                '\x1b[32m!viewarchive <month> - View historical rankings\x1b[0m\n\n' +
+                '\x1b[32m!profile <user>\x1b[0m - View user stats and progress\n' +
+                '\x1b[32m!viewarchive <month>\x1b[0m - View historical rankings\n\n' +
+
                 '\x1b[37m=== ARCADE CHALLENGE ===\x1b[0m\n' +
-                '\x1b[32m!arcade - View all arcade games and scores\x1b[0m\n' +
-                '\x1b[32m!arcade <number> - View specific game rankings\x1b[0m\n' +
+                '\x1b[32m!arcade\x1b[0m - View all arcade games and scores\n' +
+                '\x1b[32m!arcade <number>\x1b[0m - View specific game rankings\n\n' +
+
                 '\x1b[37m=== REVIEWS ===\x1b[0m\n' +
-                '\x1b[32m!review read - View reviews for games\x1b[0m\n' +
-                '\x1b[32m!review write - Write reviews for games\x1b[0m\n\n' +
+                '\x1b[32m!review read\x1b[0m - View reviews for games\n' +
+                '\x1b[32m!review write <game title>\x1b[0m - Write a review for a specific game\n\n' +
+
+                '\x1b[37m=== SEARCH ===\x1b[0m\n' +
+                '\x1b[32m!search <game title>\x1b[0m - Search MobyGames for a game\n\n' +
+
                 '\x1b[37m=== GENERAL ===\x1b[0m\n' +
-                '\x1b[32m!help - Display available commands\x1b[0m\n\n' +
+                '\x1b[32m!help\x1b[0m - Display available commands\n\n' +
+
                 '\x1b[32m[Ready for input]█\x1b[0m'
             )
             .setTerminalFooter();
 
         await message.channel.send({ embeds: [embed] });
-        await shadowGame.tryShowError(message);
+
+        if (shadowGame) {
+            await shadowGame.tryShowError(message);
+        }
     }
 };
