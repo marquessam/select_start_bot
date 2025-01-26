27ecb16e778797b9a725a89fd99d5747d2a4e168
marquessam <samuelnmarques@gmail.com>
2025-01-26 15:04:48 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,3 @@
-// commands/help.js
 const TerminalEmbed = require('../utils/embedBuilder');
 
 module.exports = {
@@ -9,38 +8,32 @@ module.exports = {
 
         const embed = new TerminalEmbed()
             .setTerminalTitle('SELECT START TERMINAL')
-            .setTerminalDescription(
-                '\x1b[37m=== CHALLENGE INFO ===\x1b[0m\n' +
-                '\x1b[32m!challenge\x1b[0m - Display current monthly challenge\n' +
-                '\x1b[32m!leaderboard\x1b[0m - View leaderboards (month/year)\n' +
-                '\x1b[32m!nominations\x1b[0m - Display nominated games\n\n' +
-
-                '\x1b[37m=== USER STATS ===\x1b[0m\n' +
-                '\x1b[32m!profile <user>\x1b[0m - View user stats and progress\n' +
-                '\x1b[32m!viewarchive <month>\x1b[0m - View historical rankings\n\n' +
-
-                '\x1b[37m=== ARCADE CHALLENGE ===\x1b[0m\n' +
-                '\x1b[32m!arcade\x1b[0m - View all arcade games and scores\n' +
-                '\x1b[32m!arcade <number>\x1b[0m - View specific game rankings\n\n' +
-
-                '\x1b[37m=== REVIEWS ===\x1b[0m\n' +
-                '\x1b[32m!review read\x1b[0m - View reviews for games\n' +
-                '\x1b[32m!review write <game title>\x1b[0m - Write a review for a specific game\n\n' +
-
-                '\x1b[37m=== SEARCH ===\x1b[0m\n' +
-                '\x1b[32m!search <game title>\x1b[0m - Search MobyGames for a game\n\n' +
-
-                '\x1b[37m=== GENERAL ===\x1b[0m\n' +
-                '\x1b[32m!help\x1b[0m - Display available commands\n\n' +
-
-                '\x1b[32m[Ready for input]█\x1b[0m'
+            .setTerminalDescription('[AVAILABLE COMMANDS]')
+            .addTerminalField('CHALLENGE & RULES',
+                '!rules - View all rules and information\n' +
+                '!profile <user> - View user stats and progress\n' +
+                '!leaderboard - View current rankings\n' +
+                '!viewarchive <month> - View historical rankings'
+            )
+            .addTerminalField('ARCADE & REVIEWS', 
+                '!arcade - View arcade games and scores\n' +
+                '!arcade <number> - View specific game rankings\n' +
+                '!review read - Browse game reviews\n' +
+                '!review write <game> - Submit a game review'
+            )
+            .addTerminalField('NOMINATIONS & SEARCH',
+                '!nominations view - View current nominations\n' +
+                '!nominations add <platform> <game> - Submit nomination\n' +
+                '!search <game> - Search for game information'
+            )
+            .addTerminalField('NOTES',
+                '- All commands must be used in #bot-terminal\n' +
+                '- Discord roles required for some features\n' +
+                '- Use !rules for detailed information'
             )
             .setTerminalFooter();
 
         await message.channel.send({ embeds: [embed] });
-
-        if (shadowGame) {
-            await shadowGame.tryShowError(message);
-        }
+        if (shadowGame) await shadowGame.tryShowError(message);
     }
 };
